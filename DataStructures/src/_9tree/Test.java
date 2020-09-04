7b6d9103416c5d10d392de3683f3f1de7b410151
‘mxg133’ <396330646@qq.com>
2020-09-04 17:01:12 +0800

    2020-9-4

---
+++ DataStructures/src/_9tree/Test.java
@@ -0,0 +1,13 @@
+package _9tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		List<Object> list = new ArrayList<>();
+
+	}
+}
