23a28448ce56f414565b184e17b15bd039ef40cb
‘mxg133’ <396330646@qq.com>
2020-09-26 12:08:08 +0800

    2020-9-26

--- DataStructures/src/_12binarysorttree/my/BinarySortTreeDemo.java
+++ DataStructures/src/_12binarysorttree/my/BinarySortTreeDemo.java
@@ -2,26 +2,28 @@ package _12binarysorttree.my;
 
 /**
  * @author 孟享广
- * @date 2020-09-17 10:48 上午
+ * @date 2020-09-26 9:05 上午
  * @description
  */
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
-
-        BinarySortTree binarySortTree = new BinarySortTree();
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 0};
+        binarySortTree binarySortTree = new binarySortTree();
 
         for (int i = 0; i < arr.length; i++) {
             binarySortTree.add(new Node(arr[i]));
         }
 
-        binarySortTree.infixOrder();
+        binarySortTree.infixorder();
+        System.out.println("情况一：删除叶子结点");
+        binarySortTree.delNode(7);
+        binarySortTree.infixorder();
     }
 }
-class BinarySortTree {
+
+class binarySortTree {
     private Node root;
 
-    //添加 （增）
     public void add(Node node) {
         if (root == null) {
             root = node;
@@ -30,14 +32,89 @@ class BinarySortTree {
         }
     }
 
-    //遍历
-    public void infixOrder() {
-        if (root != null) {
-            root.infixOrder();
+    public void infixorder() {
+        if (root == null) {
+            System.out.println("空的；无法遍历·····");
         }else {
-            System.out.println("空！```````");
+            root.infixorder();
         }
     }
+
+    public Node search(int value) {
+        if (root == null) {
+            return null;
+        }else {
+            return root.search(value);
+        }
+    }
+
+    public Node searchParent(int value) {
+        if (root == null) {
+            return null;
+        }else {
+            return root.searchParent(value);
+        }
+    }
+
+    //
+    public int delRightTreeMin(Node node) {
+        Node target = node;
+        while (target.left != null) {
+            target = target.left;
+        }
+        delNode(target.value);
+        return target.value;
+    }
+
+
+    //删除结点
+    public void delNode(int value) {
+        if (root == null) {
+            return;
+        }else {
+            Node targetNode = search(value);
+            if (targetNode == null) {
+                return;
+            }
+            if (root.left == null && root.right == null) {
+                root = null;
+                return;
+            }
+            Node parent = searchParent(value);
+            //情况一：叶子结点
+            if (targetNode.left == null && targetNode.right == null) {
+                if (parent.left != null && parent.left.value == value) {
+                    parent.left = null;
+                }else if (parent.right != null && parent.right.value == value) {
+                    parent.right = null;
+                }
+            }else if (targetNode.left != null && targetNode.right != null) {
+                //情况三：含有左右子树
+                int minValue = delRightTreeMin(targetNode.right);
+                targetNode.value = minValue;
+            }else {
+                //情况二：只有一颗子树
+                //只有左子结点
+                if (targetNode.left != null) {
+                    if (parent.left.value == value) {
+                        parent.left = targetNode.left;
+                    } else {
+                        parent.right = targetNode.left;
+                    }
+                }else {
+                    //只有右子结点
+                    if (parent.left.value == value) {
+                        parent.left = targetNode.right;
+                    }else {
+                        parent.right = targetNode.right;
+                    }
+                }
+            }
+
+        }
+
+    }
+
 }
 
 class Node {
@@ -73,7 +150,11 @@ class Node {
         this.right = right;
     }
 
-    //添加 （增）
+    @Override
+    public String toString() {
+        return value + "";
+    }
+
     public void add(Node node) {
         if (node == null) {
             return;
@@ -84,7 +165,8 @@ class Node {
             }else {
                 this.left.add(node);
             }
-        }else {
+        }
+        if (this.value < node.value) {
             if (this.right == null) {
                 this.right = node;
             }else {
@@ -93,22 +175,49 @@ class Node {
         }
     }
 
-    //遍历
-    public void infixOrder() {
+    public void infixorder() {
         if (this.left != null) {
-            this.left.infixOrder();
+            this.left.infixorder();
         }
 
         System.out.println(this);
 
         if (this.right != null) {
-            this.right.infixOrder();
+            this.right.infixorder();
         }
     }
 
-    @Override
-    public String toString() {
-        return "Node{" +
-                "value=" + value + '}';
+    //查找要删除的结点
+    public Node search(int value) {
+        if (this.value == value) {
+            return this;
+        } else if (value < this.value) {
+            if (this.left == null) {
+                return null;
+            }
+            return this.left.search(value);
+        } else {// (this.value <= value)
+            if (this.right == null){
+                return null;
+            }
+            return this.right.search(value);
+        }
     }
+
+    //找要删除的父结点
+    public Node searchParent(int value) {
+        if ((this.left != null && this.left.value == value) ||
+                (this.right != null && this.right.value == value)) {
+            return this;
+        }else {
+            if (value < this.value && this.left != null) {
+                return this.left.searchParent(value);
+            }else if (this.value <= this.value && this.right != null) {
+                return this.right.searchParent(value);
+            }else {
+                return null;
+            }
+        }
+    }
+
 }
