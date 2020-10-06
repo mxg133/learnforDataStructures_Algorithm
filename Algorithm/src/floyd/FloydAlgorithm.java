730c2eb09acbb7f89a73db0243fde24d6dbe4afd
‘mxg133’ <396330646@qq.com>
2020-10-06 10:06:40 +0800

    2020-10-6

--- Algorithm/src/floyd/FloydAlgorithm.java
+++ Algorithm/src/floyd/FloydAlgorithm.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class FloydAlgorithm {
 
 	public static void main(String[] args) {
-		// ���Կ���ͼ�Ƿ񴴽��ɹ�
+		// 测试看看图是否创建成功
 		char[] vertex = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
-		//�����ڽӾ���
+		//创建邻接矩阵
 		int[][] matrix = new int[vertex.length][vertex.length];
 		final int N = 65535;
 		matrix[0] = new int[] { 0, 5, 7, N, N, N, 2 };
@@ -17,56 +17,56 @@ public class FloydAlgorithm {
 		matrix[4] = new int[] { N, N, 8, N, 0, 5, 4 };
 		matrix[5] = new int[] { N, N, N, 4, 5, 0, 6 };
 		matrix[6] = new int[] { 2, 3, N, N, 4, 6, 0 };
-		
-		//���� Graph ����
+
+		//创建 Graph 对象
 		Graph graph = new Graph(vertex.length, matrix, vertex);
-		//���ø��������㷨
+		//调用弗洛伊德算法
 		graph.floyd();
 		graph.show();
 	}
 
 }
 
-// ����ͼ
+// 创建图
 class Graph {
-	private char[] vertex; // ��Ŷ��������
-	private int[][] dis; // ���棬�Ӹ��������������������ľ��룬���Ľ����Ҳ�Ǳ����ڸ�����
-	private int[][] pre;// ���浽��Ŀ�궥���ǰ������
+	private char[] vertex; // 存放顶点的数组
+	private int[][] dis; // 保存，从各个顶点出发到其它顶点的距离，最后的结果，也是保留在该数组
+	private int[][] pre;// 保存到达目标顶点的前驱顶点
 
-	// ������
+	// 构造器
 	/**
-	 * 
+	 *
 	 * @param length
-	 *            ��С
+	 *            大小
 	 * @param matrix
-	 *            �ڽӾ���
+	 *            邻接矩阵
 	 * @param vertex
-	 *            ��������
+	 *            顶点数组
 	 */
 	public Graph(int length, int[][] matrix, char[] vertex) {
 		this.vertex = vertex;
 		this.dis = matrix;
 		this.pre = new int[length][length];
-		// ��pre�����ʼ��, ע���ŵ���ǰ��������±�
+		// 对pre数组初始化, 注意存放的是前驱顶点的下标
 		for (int i = 0; i < length; i++) {
 			Arrays.fill(pre[i], i);
 		}
 	}
 
-	// ��ʾpre�����dis����
+	// 显示pre数组和dis数组
 	public void show() {
 
-		//Ϊ����ʾ�����Ķ��������Ż�һ�����
+		//为了显示便于阅读，我们优化一下输出
 		char[] vertex = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
 		for (int k = 0; k < dis.length; k++) {
-			// �Ƚ�pre���������һ��
+			// 先将pre数组输出的一行
 			for (int i = 0; i < dis.length; i++) {
 				System.out.print(vertex[pre[k][i]] + " ");
 			}
 			System.out.println();
-			// ���dis�����һ������
+			// 输出dis数组的一行数据
 			for (int i = 0; i < dis.length; i++) {
-				System.out.print("("+vertex[k]+"��"+vertex[i]+"�����·����" + dis[k][i] + ") ");
+				System.out.print("("+vertex[k]+"到"+vertex[i]+"的最短路径是" + dis[k][i] + ") ");
 			}
 			System.out.println();
 			System.out.println();
@@ -74,20 +74,20 @@ class Graph {
 		}
 
 	}
-	
-	//���������㷨, �Ƚ�������⣬��������ʵ��
+
+	//弗洛伊德算法, 比较容易理解，而且容易实现
 	public void floyd() {
-		int len = 0; //�����������
-		//���м䶥������� k �����м䶥����±� [A, B, C, D, E, F, G] 
-		for(int k = 0; k < dis.length; k++) { // 
-			//��i���㿪ʼ���� [A, B, C, D, E, F, G]
+		int len = 0; //变量保存距离
+		//对中间顶点遍历， k 就是中间顶点的下标 [A, B, C, D, E, F, G]
+		for(int k = 0; k < dis.length; k++) { //
+			//从i顶点开始出发 [A, B, C, D, E, F, G]
 			for(int i = 0; i < dis.length; i++) {
-				//����j���� // [A, B, C, D, E, F, G]
+				//到达j顶点 // [A, B, C, D, E, F, G]
 				for(int j = 0; j < dis.length; j++) {
-					len = dis[i][k] + dis[k][j];// => �����i ������������� k�м䶥�㣬���� j �������
-					if(len < dis[i][j]) {//���lenС�� dis[i][j]
-						dis[i][j] = len;//���¾���
-						pre[i][j] = pre[k][j];//����ǰ������
+					len = dis[i][k] + dis[k][j];// => 求出从i 顶点出发，经过 k中间顶点，到达 j 顶点距离
+					if(len < dis[i][j]) {//如果len小于 dis[i][j]
+						dis[i][j] = len;//更新距离
+						pre[i][j] = pre[k][j];//更新前驱顶点
 					}
 				}
 			}
--- Algorithm/src/kruskal/KruskalCase.java
+++ Algorithm/src/kruskal/KruskalCase.java
@@ -4,53 +4,53 @@ import java.util.Arrays;
 
 public class KruskalCase {
 
-	private int edgeNum; //�ߵĸ���
-	private char[] vertexs; //��������
-	private int[][] matrix; //�ڽӾ���
-	//ʹ�� INF ��ʾ�������㲻����ͨ
+	private int edgeNum; //边的个数
+	private char[] vertexs; //顶点数组
+	private int[][] matrix; //邻接矩阵
+	//使用 INF 表示两个顶点不能连通
 	private static final int INF = Integer.MAX_VALUE;
-	
+
 	public static void main(String[] args) {
 		char[] vertexs = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-		//��³˹�����㷨���ڽӾ���  
-	      int matrix[][] = {
-	      /*A*//*B*//*C*//*D*//*E*//*F*//*G*/
-	/*A*/ {   0,  12, INF, INF, INF,  16,  14},
-	/*B*/ {  12,   0,  10, INF, INF,   7, INF},
-	/*C*/ { INF,  10,   0,   3,   5,   6, INF},
-	/*D*/ { INF, INF,   3,   0,   4, INF, INF},
-	/*E*/ { INF, INF,   5,   4,   0,   2,   8},
-	/*F*/ {  16,   7,   6, INF,   2,   0,   9},
-	/*G*/ {  14, INF, INF, INF,   8,   9,   0}}; 
-	      //��ҿ�����ȥ�����������ڽӾ��󣬽�������Եõ���С������.
-	      
-	      //����KruskalCase ����ʵ��
-	      KruskalCase kruskalCase = new KruskalCase(vertexs, matrix);
-	      //���������
-	      kruskalCase.print();
-	      kruskalCase.kruskal();
-	      
+		//克鲁斯卡尔算法的邻接矩阵
+		int matrix[][] = {
+				/*A*//*B*//*C*//*D*//*E*//*F*//*G*/
+				/*A*/ {   0,  12, INF, INF, INF,  16,  14},
+				/*B*/ {  12,   0,  10, INF, INF,   7, INF},
+				/*C*/ { INF,  10,   0,   3,   5,   6, INF},
+				/*D*/ { INF, INF,   3,   0,   4, INF, INF},
+				/*E*/ { INF, INF,   5,   4,   0,   2,   8},
+				/*F*/ {  16,   7,   6, INF,   2,   0,   9},
+				/*G*/ {  14, INF, INF, INF,   8,   9,   0}};
+		//大家可以在去测试其它的邻接矩阵，结果都可以得到最小生成树.
+
+		//创建KruskalCase 对象实例
+		KruskalCase kruskalCase = new KruskalCase(vertexs, matrix);
+		//输出构建的
+		kruskalCase.print();
+		kruskalCase.kruskal();
+
 	}
-	
-	//������
+
+	//构造器
 	public KruskalCase(char[] vertexs, int[][] matrix) {
-		//��ʼ���������ͱߵĸ���
+		//初始化顶点数和边的个数
 		int vlen = vertexs.length;
-		
-		//��ʼ������, ���ƿ����ķ�ʽ
+
+		//初始化顶点, 复制拷贝的方式
 		this.vertexs = new char[vlen];
 		for(int i = 0; i < vertexs.length; i++) {
 			this.vertexs[i] = vertexs[i];
 		}
-		
-		//��ʼ����, ʹ�õ��Ǹ��ƿ����ķ�ʽ
+
+		//初始化边, 使用的是复制拷贝的方式
 		this.matrix = new int[vlen][vlen];
 		for(int i = 0; i < vlen; i++) {
 			for(int j= 0; j < vlen; j++) {
 				this.matrix[i][j] = matrix[i][j];
 			}
 		}
-		//ͳ�Ʊߵ�����
+		//统计边的条数
 		for(int i =0; i < vlen; i++) {
 			for(int j = i+1; j < vlen; j++) {
 				if(this.matrix[i][j] != INF) {
@@ -58,92 +58,92 @@ public class KruskalCase {
 				}
 			}
 		}
-		
+
 	}
 	public void kruskal() {
-		int index = 0; //��ʾ��������������
-		int[] ends = new int[edgeNum]; //���ڱ���"������С������" �е�ÿ����������С�������е��յ�
-		//�����������, ����������С������
+		int index = 0; //表示最后结果数组的索引
+		int[] ends = new int[edgeNum]; //用于保存"已有最小生成树" 中的每个顶点在最小生成树中的终点
+		//创建结果数组, 保存最后的最小生成树
 		EData[] rets = new EData[edgeNum];
-		
-		//��ȡͼ�� ���еıߵļ��� �� һ����12��
+
+		//获取图中 所有的边的集合 ， 一共有12边
 		EData[] edges = getEdges();
-		System.out.println("ͼ�ıߵļ���=" + Arrays.toString(edges) + " ��"+ edges.length); //12
-		
-		//���ձߵ�Ȩֵ��С��������(��С����)
+		System.out.println("图的边的集合=" + Arrays.toString(edges) + " 共"+ edges.length); //12
+
+		//按照边的权值大小进行排序(从小到大)
 		sortEdges(edges);
-		
-		//����edges ���飬������ӵ���С��������ʱ���ж���׼������ı߷��γ��˻�·�����û�У��ͼ��� rets, �����ܼ���
+
+		//遍历edges 数组，将边添加到最小生成树中时，判断是准备加入的边否形成了回路，如果没有，就加入 rets, 否则不能加入
 		for(int i=0; i < edgeNum; i++) {
-			//��ȡ����i���ߵĵ�һ������(���)
+			//获取到第i条边的第一个顶点(起点)
 			int p1 = getPosition(edges[i].start); //p1=4
-			//��ȡ����i���ߵĵ�2������
+			//获取到第i条边的第2个顶点
 			int p2 = getPosition(edges[i].end); //p2 = 5
-			
-			//��ȡp1���������������С�������е��յ�
+
+			//获取p1这个顶点在已有最小生成树中的终点
 			int m = getEnd(ends, p1); //m = 4
-			//��ȡp2���������������С�������е��յ�
+			//获取p2这个顶点在已有最小生成树中的终点
 			int n = getEnd(ends, p2); // n = 5
-			//�Ƿ񹹳ɻ�·
-			if(m != n) { //û�й��ɻ�·
-				ends[m] = n; // ����m ��"������С������"�е��յ� <E,F> [0,0,0,0,5,0,0,0,0,0,0,0]
-				rets[index++] = edges[i]; //��һ���߼��뵽rets����
+			//是否构成回路
+			if(m != n) { //没有构成回路
+				ends[m] = n; // 设置m 在"已有最小生成树"中的终点 <E,F> [0,0,0,0,5,0,0,0,0,0,0,0]
+				rets[index++] = edges[i]; //有一条边加入到rets数组
 			}
 		}
-		//<E,F> <C,D> <D,E> <B,F> <E,G> <A,B>��
-		//ͳ�Ʋ���ӡ "��С������", ���  rets
-		System.out.println("��С������Ϊ");
+		//<E,F> <C,D> <D,E> <B,F> <E,G> <A,B>。
+		//统计并打印 "最小生成树", 输出  rets
+		System.out.println("最小生成树为");
 		for(int i = 0; i < index; i++) {
 			System.out.println(rets[i]);
 		}
-		
-		
+
+
 	}
-	
-	//��ӡ�ڽӾ���
+
+	//打印邻接矩阵
 	public void print() {
-		System.out.println("�ڽӾ���Ϊ: \n");
+		System.out.println("邻接矩阵为: \n");
 		for(int i = 0; i < vertexs.length; i++) {
 			for(int j=0; j < vertexs.length; j++) {
 				System.out.printf("%12d", matrix[i][j]);
 			}
-			System.out.println();//����
+			System.out.println();//换行
 		}
 	}
 
 	/**
-	 * ���ܣ��Ա߽���������, ð������
-	 * @param edges �ߵļ���
+	 * 功能：对边进行排序处理, 冒泡排序
+	 * @param edges 边的集合
 	 */
 	private void sortEdges(EData[] edges) {
 		for(int i = 0; i < edges.length - 1; i++) {
 			for(int j = 0; j < edges.length - 1 - i; j++) {
-				if(edges[j].weight > edges[j+1].weight) {//����
+				if(edges[j].weight > edges[j+1].weight) {//交换
 					EData tmp = edges[j];
 					edges[j] = edges[j+1];
 					edges[j+1] = tmp;
 				}
 			}
- 		}
+		}
 	}
 	/**
-	 * 
-	 * @param ch �����ֵ������'A','B'
-	 * @return ����ch�����Ӧ���±꣬����Ҳ���������-1
+	 *
+	 * @param ch 顶点的值，比如'A','B'
+	 * @return 返回ch顶点对应的下标，如果找不到，返回-1
 	 */
 	private int getPosition(char ch) {
 		for(int i = 0; i < vertexs.length; i++) {
-			if(vertexs[i] == ch) {//�ҵ�
+			if(vertexs[i] == ch) {//找到
 				return i;
 			}
 		}
-		//�Ҳ���,����-1
+		//找不到,返回-1
 		return -1;
 	}
 	/**
-	 * ����: ��ȡͼ�бߣ��ŵ�EData[] �����У�����������Ҫ����������
-	 * ��ͨ��matrix �ڽӾ�������ȡ
-	 * EData[] ��ʽ [['A','B', 12], ['B','F',7], .....]
+	 * 功能: 获取图中边，放到EData[] 数组中，后面我们需要遍历该数组
+	 * 是通过matrix 邻接矩阵来获取
+	 * EData[] 形式 [['A','B', 12], ['B','F',7], .....]
 	 * @return
 	 */
 	private EData[] getEdges() {
@@ -159,10 +159,10 @@ public class KruskalCase {
 		return edges;
 	}
 	/**
-	 * ����: ��ȡ�±�Ϊi�Ķ�����յ�(), ���ں����ж�����������յ��Ƿ���ͬ
-	 * @param ends �� ������Ǽ�¼�˸��������Ӧ���յ����ĸ�,ends �������ڱ��������У����γ�
-	 * @param i : ��ʾ����Ķ����Ӧ���±�
-	 * @return ���صľ��� �±�Ϊi����������Ӧ���յ���±�, һ���ͷ���������
+	 * 功能: 获取下标为i的顶点的终点(), 用于后面判断两个顶点的终点是否相同
+	 * @param ends ： 数组就是记录了各个顶点对应的终点是哪个,ends 数组是在遍历过程中，逐步形成
+	 * @param i : 表示传入的顶点对应的下标
+	 * @return 返回的就是 下标为i的这个顶点对应的终点的下标, 一会回头还有来理解
 	 */
 	private int getEnd(int[] ends, int i) { // i = 4 [0,0,0,0,5,0,0,0,0,0,0,0]
 		while(ends[i] != 0) {
@@ -170,25 +170,25 @@ public class KruskalCase {
 		}
 		return i;
 	}
- 
+
 }
 
-//����һ����EData �����Ķ���ʵ���ͱ�ʾһ����
+//创建一个类EData ，它的对象实例就表示一条边
 class EData {
-	char start; //�ߵ�һ����
-	char end; //�ߵ�����һ����
-	int weight; //�ߵ�Ȩֵ
-	//������
+	char start; //边的一个点
+	char end; //边的另外一个点
+	int weight; //边的权值
+	//构造器
 	public EData(char start, char end, int weight) {
 		this.start = start;
 		this.end = end;
 		this.weight = weight;
 	}
-	//��дtoString, �����������Ϣ
+	//重写toString, 便于输出边信息
 	@Override
 	public String toString() {
 		return "EData [<" + start + ", " + end + ">= " + weight + "]";
 	}
-	
-	
+
+
 }
